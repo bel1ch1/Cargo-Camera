4ff31b155654595c6d53d8bbe1b96df04e74a50e
bel1c1 <andreyzv5555@gmail.com>
2024-04-13 16:27:41 +0300

    added centered funcs

--- app/backend/src/detection/detection.py
+++ app/backend/src/detection/detection.py
@@ -21,13 +21,16 @@ arucoParam = cv2.aruco.DetectorParameters()
 
 
 def center_coords(shape = FRAME_SHAPE) -> list:
+    """Изменяет точку отсчета координат"""
     center_X = FRAME_SHAPE[0] / 2
     center_Y = FRAME_SHAPE[1] / 2
     return [center_X, center_Y]
 
+
+
 # Поиск позиции
 def pose_esitmation(img, arucoDict, centerX, centerY):
-    """returns coordinates and distance to the object"""
+    """Возвращает координаты углов маркера и дистанцию до него"""
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -43,21 +46,44 @@ def pose_esitmation(img, arucoDict, centerX, centerY):
         # Определяем дистанцию до маркера
         distance_to_marker = np.linalg.norm(tvec)
 
-        # координаты и дистанция
+        # координаты X
         c1X = corners[0][0][0][0] - centerX
         c2X = corners[0][0][1][0] - centerX
         c3X = corners[0][0][2][0] - centerX
         c4X = corners[0][0][3][0] - centerX
+
+        # координаты Y
         c1Y = corners[0][0][0][1] - centerY
         c2Y = corners[0][0][1][1] - centerY
         c3Y = corners[0][0][2][1] - centerY
         c4Y = corners[0][0][3][1] - centerY
+
+        # дистанция
         d = distance_to_marker
 
         # Вывод в консоль
         print(f"{c1X, c1Y}, {c2X, c2Y}, {c3X, c3Y}, {c4X, c4Y}, \ndistance: {d}")
         # Возврат значений
         return f"1:{c1X, c1Y}, 2:{c2X, c2Y}, 3:{c3X, c3Y}, 4:{c4X, c4Y}, \ndistance: {d}"
+
+def calibrate_centered_marker(img, arucoDict, centerX, centerY ) -> list:
+    """находит координаты отцентрованного маркера.
+    Возвращает найденные координаты"""
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Ищем маркеры
+    corners, _, _ = cv2.aruco.detectMarkers(gray, arucoDict, parameters=arucoParam)
+
+    # если нашли
+    if corners:
+        calibrate = True
+        return[calibrate, corners]
+
+
+def set_valid_area(valid_area: int ,calibrate_status, **centered_corners: str):
+    """У станавливает допустимую область отклонения маркера"""
+    pass
+
 
 
 # захват видео с камеры и обработка
