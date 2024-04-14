be6c43532d2d26e35d8ec077ccce761a181f5c10
bel1c1 <andreyzv5555@gmail.com>
2024-04-14 14:43:53 +0300

    Refactoring constances

--- app/backend/src/detection/detection.py
+++ app/backend/src/detection/detection.py
@@ -1,14 +1,23 @@
 import cv2
+import pickle
 import numpy as np
 
+from .constantes import MARKER_SIZE_M, ARUCO_PARAM
 
-FRAME_SHAPE = [640, 480]
+# Подгружаем данные для определения дистанции
+with open('calibration_params//dist.pkl', 'rb') as f:
+    dist_coef = pickle.load(f)
 
-def center_coords(shape = FRAME_SHAPE) -> list:
+with open('calibration_params//cameraMatrix.pkl', 'rb') as g:
+    cam_mat = pickle.load(g)
+
+
+def center_coords() -> list:
     """Изменяет точку отсчета координат"""
-    center_X = FRAME_SHAPE[0] / 2
-    center_Y = FRAME_SHAPE[1] / 2
+    center_X = None
+    center_Y = None
     return [center_X, center_Y]
+
 
 # Поиск позиции
 def pose_esitmation(img, arucoDict, centerX, centerY):
@@ -17,13 +26,15 @@ def pose_esitmation(img, arucoDict, centerX, centerY):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Ищем маркеры
-    corners, _, _ = cv2.aruco.detectMarkers(gray, arucoDict, parameters=arucoParam)
+    corners, _, _ = cv2.aruco.detectMarkers(gray, arucoDict, parameters=ARUCO_PARAM)
 
     # если нашли
     if corners:
 
         # Определяем сдвиг
-        _ , tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, MARKER_SIZE_M, cam_mat, dist_coef)
+        _ , tvec, _ = cv2.aruco.estimatePoseSingleMarkers(
+            corners, MARKER_SIZE_M, cam_mat, dist_coef
+        )
 
         # Определяем дистанцию до маркера
         distance_to_marker = np.linalg.norm(tvec)
@@ -44,9 +55,12 @@ def pose_esitmation(img, arucoDict, centerX, centerY):
         d = distance_to_marker
 
         # Вывод в консоль
-        print(f"{c1X, c1Y}, {c2X, c2Y}, {c3X, c3Y}, {c4X, c4Y}, \ndistance: {d}")
+        print(
+            f"{c1X, c1Y}, {c2X, c2Y}, {c3X, c3Y}, {c4X, c4Y}, \ndistance: {d}"
+            )
         # Возврат значений
         return f"1:{c1X, c1Y}, 2:{c2X, c2Y}, 3:{c3X, c3Y}, 4:{c4X, c4Y}, \ndistance: {d}"
+
 
 def calibrate_centered_marker(img, arucoDict, centerX, centerY ) -> list:
     """находит координаты отцентрованного маркера.
@@ -60,6 +74,7 @@ def calibrate_centered_marker(img, arucoDict, centerX, centerY ) -> list:
     if corners:
         calibrate = True
         return[calibrate, corners]
+
 
 def set_valid_area(valid_area: int ,calibrate_status, **centered_corners: str):
     """У станавливает допустимую область отклонения маркера"""
