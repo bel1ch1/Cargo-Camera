a7d0beabbc0487c31d19151185b7870227d24e9e
bel1c1 <andreyzv5555@gmail.com>
2024-04-29 15:22:05 +0300

    add entry point for Launcher

--- app/backend/src/Launcher/main.py
+++ app/backend/src/Launcher/main.py
@@ -1,15 +1,24 @@
-# Скрипт для запуска программы детекции
-import subprocess
+# # Точка входа для скрипта
+# import subprocess
 
 
-cd = subprocess.run(["cd"])
+# cd = subprocess.run(["cd"])
 
-computer_name = subprocess.run(["whoami"])
+# computer_name = subprocess.run(["whoami"])
 
-path_to_detection = subprocess.run(
-    [f"cd /home/{computer_name}/Cargo/Cargo-Camera/app/backend/src/detection"]
-)
+# path_to_detection = subprocess.run(
+#     [f"cd /home/{computer_name}/Cargo/Cargo-Camera/app/backend/src/detection"]
+# )
 
-run_res = subprocess.run(["python3"], input="main.py", capture_output=True)
+# run_res = subprocess.run(["python3"], input="main.py", capture_output=True)
 
-print(run_res.stdout.decode())
+# print(run_res.stdout.decode())
+from req import start
+
+
+def main():
+    start()
+
+
+if __name__ == "__main__":
+    main()
