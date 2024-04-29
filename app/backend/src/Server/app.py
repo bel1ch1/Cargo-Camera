8a78ddc987385d2086a8ab43ecbc8a8a1a8a7215
bel1c1 <andreyzv5555@gmail.com>
2024-04-29 14:51:38 +0300

    fastapi app added route for start

--- app/backend/src/Server/app.py
+++ app/backend/src/Server/app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from fastapi.responses import RedirectResponse
+from fastapi.responses import HTMLResponse
 from websockets.exceptions import ConnectionClosed
 from fastapi.templating import Jinja2Templates
 import uvicorn
@@ -11,11 +13,28 @@ camera = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get('/')
-def index(request: Request):
+# Рендер стартовой страницы
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+# Кнопка запуска приложения для детекции
+@app.get('/start_detection')
+async def start_detection_app(request: Request):
+    """Отвечает на запросс для старта приложения детекции"""
+    request = {"status": "True"}
+    return request
+
+
+# Калибровка позиции для пользователя
+@app.get('/calibration')
+def calibration(request: Request):
+    """Выводит картинку для пользователя, чтобы откалибровать положение груза"""
     return templates.TemplateResponse("calibration.html", {"request": request})
 
 
+# Апгрейд протокола до websocket-а
 @app.websocket("/ws")
 async def get_stream(websocket: WebSocket):
     await websocket.accept()
@@ -25,11 +44,15 @@ async def get_stream(websocket: WebSocket):
             if not success:
                 break
             else:
-                ret, buffer = cv2.imencode('.jpg', frame)
+                _, buffer = cv2.imencode('.jpg', frame)
                 await websocket.send_bytes(buffer.tobytes())
             await asyncio.sleep(0.03)
     except (WebSocketDisconnect, ConnectionClosed):
         print("Client disconnected")
+    finally:
+        await websocket.close()
+        RedirectResponse("/")
 
+# Точка входа
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000) # менять
