368c0c3ca7fe62f8fd978fdb969d50d9228b73ee
bel1c1 <andreyzv5555@gmail.com>
2024-04-29 12:58:18 +0300

    script for starting detection

--- app/backend/src/Launcher/main.py
+++ app/backend/src/Launcher/main.py
@@ -1,3 +1,15 @@
+# Скрипт для запуска программы детекции
 import subprocess
 
-result = subprocess.run(["ls"])
+
+cd = subprocess.run(["cd"])
+
+computer_name = subprocess.run(["whoami"])
+
+path_to_detection = subprocess.run(
+    [f"cd /home/{computer_name}/Cargo/Cargo-Camera/app/backend/src/detection"]
+)
+
+run_res = subprocess.run(["python3"], input="main.py", capture_output=True)
+
+print(run_res.stdout.decode())
