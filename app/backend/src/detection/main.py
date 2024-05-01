7709734c776ed4f071b1df37cfe42484053939b5
bel1c1 <andreyzv5555@gmail.com>
2024-05-01 17:17:26 +0300

    for showcase

--- app/backend/src/detection/main.py
+++ app/backend/src/detection/main.py
@@ -14,7 +14,7 @@ cb_pos = []
 while not cb_pos:
     ret, frame= cap.read()
     if ret:
-        cv2.imshow('cb', frame)
+        #cv2.imshow('cb', frame)
         k = cv2.waitKey(10) & 0xff
         if k == ord('s'):
             cb_pos = get_centered_marker(cap, ARUCO_DICT)
@@ -41,7 +41,7 @@ while cap.isOpened():
     output=pose_esitmation(frame, ARUCO_DICT, valid_area[0], valid_area[1], valid_area[2], valid_area[3])
 
     # Вывод кадров
-    cv2.imshow("output", frame)
+    #cv2.imshow("output", frame)
 
 
     # Остановка
